3949511acca5e8c218032efcae5b5ababbaf014d
Srj0210 <sleepyspider6166@gmail.com>
2025-10-31 18:34:06 +0530

    Update animation.js

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -28,3 +28,30 @@ document.querySelectorAll("section").forEach((sec) => {
     ease: "power2.out"
   });
 });
+
+// Animate project cards on scroll
+gsap.utils.toArray(".card").forEach((card, i) => {
+  gsap.from(card, {
+    scrollTrigger: {
+      trigger: card,
+      start: "top 85%",
+      toggleActions: "play none none none"
+    },
+    opacity: 0,
+    y: 50,
+    duration: 1,
+    delay: i * 0.1,
+    ease: "power2.out"
+  });
+});
+
+// Floating logo effect for project logos
+gsap.utils.toArray(".project-logo").forEach((logo, i) => {
+  gsap.to(logo, {
+    y: 10,
+    repeat: -1,
+    yoyo: true,
+    duration: 3 + i * 0.5,
+    ease: "power1.inOut"
+  });
+});
